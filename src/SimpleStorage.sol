1565a0c5aa8aacf32e947ccf8547a09074771bbd
Hemanth1431 <clevercriminal@gmail.com>
2023-06-10 19:22:17 +0530

    simple storage

---
+++ src/SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract SimpleStorage {
+    uint256 public num;
+    address public owner;
+
+    function getOwner() public view returns(uint256 value){
+        assembly {
+            let x := num.slot
+            value := sload(x)
+        }
+    }
+}
