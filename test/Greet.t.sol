a92b692be67f707cf12fb8596fe665ca40d64c0b
Hemanth1431 <clevercriminal@gmail.com>
2023-06-13 07:47:30 +0530

    save

---
+++ test/Greet.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+import "../src/Greet.sol";
+
+contract GreetTest is Test {
+    HelloWorld public helloworld;
+
+    function setUp() public {
+        helloworld = new HelloWorld();
+    }
+
+    function testgreet() public {
+        assertEq(helloworld.greet(), "Hello World!");
+    }
+}
