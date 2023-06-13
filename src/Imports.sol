18d3fe9e0d7c52cb10825d0a1fa542158ea86903
Hemanth1431 <clevercriminal@gmail.com>
2023-06-13 08:42:17 +0530

    save

--- src/Imports.sol
+++ src/Imports.sol
@@ -4,3 +4,7 @@ pragma solidity ^0.8.13;
 import "solmate/tokens/ERC20.sol";
 
 contract Token is ERC20("name", "symble", 18){}
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestOP is Ownable {}
