bb5987ddff58ae2eba1b3ef930d2ad030207bede
Hemanth1431 <clevercriminal@gmail.com>
2023-06-16 11:30:44 +0530

    save

---
+++ test/Events.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Events.sol";
+
+contract Eventtest is Test{
+    Event public eve;
+
+    event Transfer(address indexed from, address indexed to, uint amount);
+
+    function setUp()public{
+        eve = new Event();
+    }
+
+    function testEmitTransferEvent()public{
+        vm.expectEmit(true, true, false, true);
+        emit Transfer(address(this), address(123), 456);
+        eve.transfer(address(this), address(123), 456);
+        //check index 1;
+        /*vm.expectEmit(true, false, false, false);
+        emit Transfer(address(this), address(123), 456);
+        eve.transfer(address(this), address(13), 46);
+        */
+    }
+
+
+    function testEmitTransferMany()public{
+        address[] memory to = new address[](2);
+        to[0] = address(1783);
+        to[1] = address(8907);
+
+        uint[]memory amounts = new uint256[](2);
+        amounts[0] = 7890;
+        amounts[1] = 6784; 
+
+        for(uint i; i < to.length; i++){
+            vm.expectEmit(true, true, false, true);
+            emit Transfer(address(this), to[i], amounts[i]);
+        }
+
+        eve.transferMany(address(this), to, amounts);
+    }
+}
---
+++ test/TimeStamp.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/TimeStamp.sol";
+
+contract TimeTest is Test{
+    Auction public auction;
+    uint256 private startAt;
+    function setUp()public{
+        auction = new Auction();
+        startAt = block.timestamp;
+    }
+    //vm.warp() = set block.timestamp to future timestamp
+    // vm.roll() = set block.number
+    //skip() = increase current timestamp
+    //rewind = decrease current timestamp
+
+    function testBidFailsBeforeStartTime()public{
+        vm.expectRevert(bytes("cannot revert"));
+        auction.bid();
+    }
+
+    function testBid()public{
+        vm.warp(startAt + 1 days);
+        auction.bid();
+    }
+    function testBidFailsAfterEndTime()public{
+        vm.expectRevert(bytes("cannot revert"));
+        vm.warp(startAt + 2 days);
+        auction.bid();
+    }
+
+    function testTimeStamp()public{
+        uint t = block.timestamp;
+        skip(100);
+        assertEq(block.timestamp, t + 100);
+
+        rewind(10);
+        assertEq(block.timestamp, t + 100 - 10);
+    }
+
+    function testBlockNumber()public{
+        uint b = block.number;
+        vm.roll(5);
+        assertEq(block.number, 5);
+    }
+
+
+
+}
